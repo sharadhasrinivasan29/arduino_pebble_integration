2d0369b2cb455466ed809126ec741619f98ae7f9
dy-choi <dchoi0920@gmail.com>
2017-04-19 11:11:39 -0400

    connection to server

--- temperature.ino
+++ temperature.ino
@@ -35,7 +35,7 @@ Change History:
 const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
   0x6D,0x7D,0x07,0x7F,0x6F,
   0x77,0x7C,0x39,0x5E,0x79,0x71};
-  
+
   /* Function prototypes */
   void convert_c_to_f (int&, byte&, bool&, int, byte, bool&);
   void Cal_temp (int&, byte&, byte&, bool&);
@@ -44,14 +44,14 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
   void SerialMonitorPrint (byte, int, bool);
   void UpdateRGB (byte);
   int incomingByte;
-  
+
   /***************************************************************************
   Function Name: setup
-  
+
   Purpose:
   Initialize hardwares.
   ****************************************************************************/
-  
+
   void setup()
   {
     Serial.begin(BAUD);
@@ -62,14 +62,14 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
     pinMode(LED_BUILTIN, OUTPUT);
     delay(500);          /* Allow system to stabilize */
   }
-  
+
   /***************************************************************************
   Function Name: loop
-  
+
   Purpose:
   Run-time forever loop.
   ****************************************************************************/
-  
+
   void loop()
   {
     //  String msg = ""; // DC
@@ -78,41 +78,42 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
     byte Temperature_H, Temperature_L, counter, counter2;
     bool IsPositive;
     bool stop = 0;
-    
+
     // DC temperature conversion
-    bool celsius = 1; // DC
+    bool celsius = true; // DC
+    bool standby = false;
     bool f_IsPositive = 0;
     int f_decimal = 0;
     byte f_temperature_H = 0;
     byte f_temperature_L = 0;
-    
-    
+
+
     /* Configure 7-Segment to 12mA segment output current, Dynamic mode,
     and Digits 1, 2, 3 AND 4 are NOT blanked */
-    
+
     Wire.beginTransmission(_7SEG);
     byte val = 0;
     Wire.write(val);
     val = B01000111;
     Wire.write(val);
     Wire.endTransmission();
-    
+
     /* Setup configuration register 12-bit */
-    
+
     Wire.beginTransmission(THERM);
     val = 1;
     Wire.write(val);
     val = B01100000;
     Wire.write(val);
     Wire.endTransmission();
-    
+
     /* Setup Digital THERMometer pointer register to 0 */
-    
+
     Wire.beginTransmission(THERM);
     val = 0;
     Wire.write(val);
     Wire.endTransmission();
-    
+
     /* Test 7-Segment */
     for (counter=0; counter<8; counter++)
     {
@@ -125,46 +126,72 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
       Wire.endTransmission();
       delay (250);
     }
-    
+
     while (1)
     {
       Wire.requestFrom(THERM, 2);
       Temperature_H = Wire.read();
       Temperature_L = Wire.read();
-      
+
       /* Calculate temperature */
       Cal_temp (Decimal, Temperature_H, Temperature_L, IsPositive);
-      
+
       /* Display temperature on the serial monitor.
       Comment out this line if you don't use serial monitor.*/
       if (stop == 0) {
         SerialMonitorPrint (Temperature_H, Decimal, IsPositive);
       }
-      
+
       /* Update RGB LED.*/
       UpdateRGB (Temperature_H);
-      
+
       if (Serial.available() > 0) {
+        // handle commands from server
         incomingByte = Serial.read();
+        if (incomingByte == 49) { // TODO : celsius
+          celsius = true;
+        }
+        else if (incomingByte == 9990) { // TODO : temp f
+          celsius = false;
+        }
+        else if (incomingByte == 9991) { // TODO : standby
+          standby = true;
+        }
+        else if (incomingByte == 9992) { // TODO : resume from standby
+          standby = false;
+        }
+        else if (incomingByte == 9993) {
+          // TODO : other features
+        }
+
       }
-      
+
       /* Display temperature on the 7-Segment */
-      if (celsius == 0) { // DC : display in F
+      if (standby == true) {
+        for (counter = 1; counter <= 4; counter++) {
+          Send7SEG(counter,0x40);
+        }
+        Serial.print("---Standby---\n");
+
+        delay(1000);
+      }
+
+      else if (celsius == true) { // DC : display in C
+        Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, 1);
+      } else { // DC: display in F
         convert_c_to_f(f_decimal, f_temperature_H, f_IsPositive, Decimal, Temperature_H, IsPositive);
         Dis_7SEG(f_decimal, f_temperature_H, f_temperature_L, f_IsPositive, 0);
-      } else { // DC: display in C
-        Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, 1);
       }
-      
+
       delay (1000);        /* Take temperature read every 1 second */
     }
   }
-  
-  
+
+
   /***************************************************************************
   Function Name: convert_c_to_f
   // DC
-  
+
   Purpose:
   Calculate temperature from raw data.
   ****************************************************************************/
@@ -183,10 +210,10 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
     f_high = (int) deg_f;
     f_decimal = (int) ((deg_f - f_high) * 1000);
   }
-  
+
   /***************************************************************************
   Function Name: Cal_temp
-  
+
   Purpose:
   Calculate temperature from raw data.
   ****************************************************************************/
@@ -196,23 +223,23 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
     sign = 0;
     else
     sign = 1;
-    
+
     High = High & B01111111;      /* Remove sign bit */
     Low = Low & B11110000;        /* Remove last 4 bits */
     Low = Low >> 4;
     Decimal = Low;
     Decimal = Decimal * 625;      /* Each bit = 0.0625 degree C */
-    
+
     if (sign == 0)                /* if temperature is negative */
     {
       High = High ^ B01111111;    /* Complement all of the bits, except the MSB */
       Decimal = Decimal ^ 0xFF;   /* Complement all of the bits */
     }
   }
-  
+
   /***************************************************************************
   Function Name: Dis_7SEG
-  
+
   Purpose:
   Display number on the 7-segment display.
   ****************************************************************************/
@@ -220,13 +247,13 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
   {
     byte Digit = 4;                 /* Number of 7-Segment digit */
     byte Number;                    /* Temporary variable hold the number to display */
-    
+
     if (sign == 0)                  /* When the temperature is negative */
     {
       Send7SEG(Digit,0x40);         /* Display "-" sign */
       Digit--;                      /* Decrement number of digit */
     }
-    
+
     if (High > 99)                  /* When the temperature is three digits long */
     {
       Number = High / 100;          /* Get the hundredth digit */
@@ -234,7 +261,7 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
       High = High % 100;            /* Remove the hundredth digit from the TempHi */
       Digit--;                      /* Subtract 1 digit */
     }
-    
+
     if (High > 9)
     {
       Number = High / 10;           /* Get the tenth digit */
@@ -242,7 +269,7 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
       High = High % 10;            /* Remove the tenth digit from the TempHi */
       Digit--;                      /* Subtract 1 digit */
     }
-    
+
     Number = High;                  /* Display the last digit */
     Number = NumberLookup [Number];
     if (Digit > 1)                  /* Display "." if it is not the last digit on 7-SEG */
@@ -251,14 +278,14 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
     }
     Send7SEG (Digit,Number);
     Digit--;                        /* Subtract 1 digit */
-    
+
     if (Digit > 0)                  /* Display decimal point if there is more space on 7-SEG */
     {
       Number = Decimal / 1000;
       Send7SEG (Digit,NumberLookup[Number]);
       Digit--;
     }
-    
+
     //  if(incomingByte == 49) {
     if (Digit > 0)                 /* Display "c" if there is more space on 7-SEG */
     {
@@ -270,22 +297,22 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
       Digit--;
     }
     //  }
-    
-    
+
+
     if (Digit > 0)                 /* Clear the rest of the digit */
     {
       Send7SEG (Digit,0x00);
     }
-    
+
   }
-  
+
   /***************************************************************************
   Function Name: Send7SEG
-  
+
   Purpose:
   Send I2C commands to drive 7-segment display.
   ****************************************************************************/
-  
+
   void Send7SEG (byte Digit, byte Number)
   {
     Wire.beginTransmission(_7SEG);
@@ -293,20 +320,20 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
     Wire.write(Number);
     Wire.endTransmission();
   }
-  
+
   /***************************************************************************
   Function Name: UpdateRGB
-  
+
   Purpose:
   Update RGB LED according to define HOT and COLD temperature.
   ****************************************************************************/
-  
+
   void UpdateRGB (byte Temperature_H)
   {
     digitalWrite(RED, LOW);
     digitalWrite(GREEN, LOW);
     digitalWrite(BLUE, LOW);        /* Turn off all LEDs. */
-    
+
     if (Temperature_H <= COLD)
     {
       digitalWrite(BLUE, HIGH);
@@ -320,10 +347,10 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
       digitalWrite(GREEN, HIGH);
     }
   }
-  
+
   /***************************************************************************
   Function Name: SerialMonitorPrint
-  
+
   Purpose:
   Print current read temperature to the serial monitor.
   ****************************************************************************/
