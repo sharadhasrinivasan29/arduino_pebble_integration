176c393bb284db1680fb60ef78c563289b631843
dy-choi <dchoi0920@gmail.com>
2017-04-23 15:40:08 -0400

    in the process of changing keys

--- temperature.ino
+++ temperature.ino
@@ -66,7 +66,7 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
 
   void loop()
   {
-    //  String msg = ""; // DC
+    String msg = ""; // DC
     int Decimal;
     byte Temperature_H, Temperature_L, counter, counter2;
     bool IsPositive;
@@ -78,7 +78,7 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
     bool blinking = false; // feature 3
 
     // DC temperature conversion
-    bool celsius = true;
+    bool celsius = false;
     bool standby = false;
     bool f_IsPositive = 0;
     int f_decimal = 0;
@@ -160,15 +160,20 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
       max_temp_dec = INT_MIN;
       min_temp_h = INT_MAX;
       min_temp_dec = INT_MAX;
+      double current_temp = Temperature_H + ((double) Decimal / 100);
+      double max_temp = -1000;
+      double min_temp = 1000;
       avg_temp_h = 0;
       avg_temp_dec = 0;
 
       for (int i = 0; i < j; i++) {
-        if (temperature_stats[i][0] < min_temp_h && temperature_stats[i][1] < min_temp_dec) {
+        if (current_temp < min_temp) {
+          min_temp = current_temp;
           min_temp_h = temperature_stats[i][0];
           min_temp_dec = temperature_stats[i][1];
         }
-        if (temperature_stats[i][0] > max_temp_h && temperature_stats[i][1] > max_temp_dec) {
+        if (current_temp > max_temp) {
+          max_temp = current_temp;
           max_temp_h = temperature_stats[i][0];
           max_temp_dec = temperature_stats[i][1];
         }
@@ -207,227 +212,280 @@ const byte NumberLookup[16] =   {0x3F,0x06,0x5B,0x4F,0x66,
         digitalWrite(BLUE, LOW);
       }
 
-      //      if (Serial.available() > 0) {
-      // handle commands from server
-      incomingByte = 50;
+      if (Serial.available() > 0) {
+        // handle commands from server
+        //        incomingByte = Serial.read(); // DC commented out
+        msg = Serial.readString();
 
-      //        incomingByte = Serial.read(); // DC commented out TODO uncomment
-      if (incomingByte == 49) {
-        celsius = true;
-        writing_word = false; // TODO maybe delete?
-        blinking = false;
-        showing_trend = false;
-        Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, 1);
-        SerialMonitorPrint (Temperature_H, Decimal, IsPositive, true);
-      }
-      if (incomingByte == 9990) { // TODO : temp f
-        celsius = false;
-        writing_word = false;
-        blinking = false;
-        showing_trend = false;
-        Dis_7SEG(f_decimal * 10, f_temperature_H, f_temperature_L, f_IsPositive, 0);
-        SerialMonitorPrint(f_temperature_H, f_decimal, f_IsPositive, false);
-      }
-      if (incomingByte == 9995) { // TODO : stats
-        writing_word = false;
-        blinking = false;
-        showing_trend = true; // TODO do we want this?
-        // TODO convert to F if necessary
-        if (celsius) {
-          Serial.print("max : ");
-          Serial.print(max_temp_h, DEC);
-          Serial.print(".");
-          Serial.println(max_temp_dec, DEC);
-
-          Serial.print("min : ");
-          Serial.print(min_temp_h, DEC);
-          Serial.print(".");
-          Serial.println(min_temp_dec, DEC);
-
-          Serial.print("avg : ");
-          Serial.print(avg_temp_h, DEC);
-          Serial.print(".");
-          if (avg_temp_dec < 10) {
-            Serial.print(0, DEC);
+        if (msg.equals("02")) {
+          celsius = true;
+          writing_word = false; // TODO maybe delete?
+          blinking = false;
+          showing_trend = false;
+          Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, 1);
+          SerialMonitorPrint (Temperature_H, Decimal, IsPositive, true);
+        }
+        if (msg.equals("01")){
+          celsius = false;
+          writing_word = false;
+          blinking = false;
+          showing_trend = false;
+          Dis_7SEG(f_decimal * 10, f_temperature_H, f_temperature_L, f_IsPositive, 0);
+          SerialMonitorPrint(f_temperature_H, f_decimal, f_IsPositive, false);
+        }
+        if (msg.equals("03")) {
+          // avg temp
+          writing_word = false;
+          blinking = false;
+          showing_trend = false;
+          if (celsius) {
+            Serial.print(avg_temp_h, DEC);
+            Serial.print(".");
+            if (avg_temp_dec < 10) {
+              Serial.print(0, DEC);
+            }
+            Serial.print(avg_temp_dec, DEC);
+            Serial.println(" C.");
+          } else {
+            int convert_decimal;
+            byte convert_high;
+            bool convert_sign;
+            convert_c_to_f (convert_decimal, convert_high, convert_sign, avg_temp_dec, avg_temp_h, IsPositive);
+            Serial.print(convert_high, DEC);
+            Serial.print(".");
+            if (convert_decimal < 10) {
+              Serial.print(0, DEC);
+            }
+            Serial.print(convert_decimal, DEC);
+            Serial.println(" F.");
           }
-          Serial.println(avg_temp_dec, DEC);
+
+        }
+        if (msg.equals("04")) {
+          // TODO max
+          writing_word = false;
+          blinking = false;
+          showing_trend = false;
+          if (celsius) {
+            Serial.print("max : ");
+            Serial.print(max_temp_h, DEC);
+            Serial.print(".");
+            Serial.println(max_temp_dec, DEC);
+          } else {
+            int convert_decimal;
+            byte convert_high;
+            bool convert_sign;
+
+            convert_c_to_f (convert_decimal, convert_high, convert_sign, max_temp_dec, max_temp_h, IsPositive);
+            Serial.print("max : ");
+            Serial.print(convert_high, DEC);
+            Serial.print(".");
+            if (convert_decimal < 10) {
+              Serial.print(0, DEC);
+            }
+            Serial.println(convert_decimal, DEC);
+          }
+        }
+        if (msg.equals("05")) {
+          // TODO min
+          writing_word = false;
+          blinking = false;
+          showing_trend = false;
+          if (celsius) {
+            Serial.print("min : ");
+            Serial.print(min_temp_h, DEC);
+            Serial.print(".");
+            Serial.println(min_temp_dec, DEC);
+          } else {
+            int convert_decimal;
+            byte convert_high;
+            bool convert_sign;
+
+            convert_c_to_f (convert_decimal, convert_high, convert_sign, min_temp_dec, min_temp_h, IsPositive);
+            Serial.print("min : ");
+            Serial.print(convert_high, DEC);
+            Serial.print(".");
+            if (convert_decimal < 10) {
+              Serial.print(0, DEC);
+            }
+            Serial.println(convert_decimal, DEC);
+          }
+        }
+        if (msg.equals("06")) {
+          // standby
+          standby = true;
+          blinking = false;
+          writing_word = true; // TODO check
+          showing_trend = false;
+          for (counter = 1; counter <= 4; counter++) {
+            Send7SEG(counter,0x40); // print ----
+          }
+          Serial.print("--- standby ---");
+
+        }
+        if (msg.equals("13")) {
+          // resume from standby
+          standby = false;
+          writing_word = false;
+          celsius = true;
+          blinking = false;
+          showing_trend = false;
+          Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, 1);
+          SerialMonitorPrint (Temperature_H, Decimal, IsPositive, true);
+        }
+
+        if (msg.equals("07")) {
+          // TODO trend
+          writing_word = false;
+          blinking = false;
+          showing_trend = true;
+          long xsum;
+          long ysum;
+          long xxsum;
+          long xysum;
+          double mNumerator;
+          double mDenominator;
+          double m;
+
+          //calculate summations over the past hour
+          for (int i = 0; i < j; i++){
+            ysum = ysum + temperature_stats[i][0];
+            xsum = xsum + i;
+            xysum = xysum + (i * temperature_stats[index][0]);
+            xxsum = xxsum + (i*i);
+          }
+
+          //calculate slope of regression
+          mNumerator = (j * xysum) - (xsum * ysum);
+          mDenominator = (j * xxsum) - (xsum * xsum);
+          m = mNumerator / mDenominator;
+
+          //send trend to computer
+          if (m > 0){
+            Serial.println("The temperature trend is increasing.");
+          } else if (m < 0){
+            Serial.println("The temperature trend is decreasing.");
+          } else {
+            Serial.println("The temperature has remained constant.");
+          }
+        }
+
+        if (msg.equals("08")) {
+          //  Serial monitor : OoPS
+          writing_word = true;
+          showing_trend = false;
+          Word_7SEG(0, 1, 2, 3);
+          //          writing_word = true;
+          //          showing_trend = false;
+          //          Word_7SEG(4, 5, 6, 0); //  Hello
+          //          writing_word = true;
+          //          blinking = false;
+          //          showing_trend = false;
+          //          Word_7SEG(4, 0, 11, 10); //  HOLA
+        }
+
+        if (msg.equals("09")) { // blink green
+          writing_word = false;
+          blinking = true;
+          showing_trend = false;
+          digitalWrite(GREEN, HIGH);
+          delay(1000);                       // wait for a second
+          digitalWrite(GREEN, LOW);
+          delay(1000);                       // wait for a second
+        }
+
+        if (msg.equals("10")) { // blink blue
+          writing_word = false;
+          blinking = true;
+          showing_trend = false;
+          digitalWrite(BLUE, HIGH);
+          delay(1000);                       // wait for a second
+          digitalWrite(BLUE, LOW);
+          delay(1000);                       // wait for a second
+        }
+
+
+        if (msg.equals("11")) { // blink red
+          writing_word = false;
+          blinking = true;
+          showing_trend = false;
+          digitalWrite(RED, HIGH);
+          delay(1000);                       // wait for a second
+          digitalWrite(RED, LOW);
+          delay(1000);                       // wait for a second
+        }
+
+        if (msg.equals("12")) {
+          // drink recommendation
+        }
+
+
+
+
+
+        byte tooHot = -1;
+        byte tooCold = -1;
+
+        if (incomingByte == 9999){ // TODO : feature 4 35 degrees too hot
+          showing_trend = false;
+          writing_word = false;
+          blinking = false;
+          tooHot = 35;
+        }
+
+        if (incomingByte == 9999){ // TODO: feature 4 30 degrees too hot
+          showing_trend = false;
+          writing_word = false;
+          blinking = false;
+          tooHot = 30;
+        }
+
+        if (incomingByte == 9999){ // TODO: feature 4 25 degrees too hot
+          writing_word = false;
+          showing_trend = false;
+          blinking = false;
+          tooHot = 25;
+
+        }
+
+        if (incomingByte == 9999) { // TODO: feature 4 0 is too cold
+          showing_trend = false;
+          blinking = false;
+          writing_word = false;
+          tooCold = 0;
+        }
+
+        if (incomingByte == 9999 ){ // TODO: feature 4 5 degrees is too cold
+          showing_trend = false;
+          writing_word = false;
+          blinking = false;
+          tooCold = 5;
+        }
+
+        if (incomingByte == 9999){ // TODO : feature 4 10 degrees is too cold
+          showing_trend = false;
+          writing_word = false;
+          blinking = false;
+          tooCold = 10;
+        }
+
+        if (incomingByte == 9990){ // TODO : feature 4 asking for a drink 9990 is "thirsty" option
+        showing_trend = false;
+        writing_word = false;
+        blinking = false;
+        if (tooHot == -1 || tooCold == -1) {
+          // error handling
+          Serial.print("No preference found.");
+        }
+        if (Temperature_H >= tooHot){
+          Serial.print("Have an iced tea!");
+        } else if (Temperature_H <= tooCold){
+          Serial.print("Have a hot cocoa!");
         } else {
-          //            int convert_decimal;
-          //            byte convert_high;
-          //            bool convert_sign;
-          //            // TODO change last parameter if we really want (boolean for -/+)
-          //            convert_c_to_f (int& convert_decimal, byte& convert_high, bool& convert_sign, int max_temp_dec, byte max_temp_h, 1);
-          //            Serial.print("max : ");
-          //            Serial.print(f_decimal + "." + );
-          //
+          Serial.print("Have a glass of water!");
         }
-      }
-      if (incomingByte == 9991) { // TODO : standby
-        standby = true;
-        blinking = false;
-        writing_word = true; // TODO check
-        showing_trend = false;
-        for (counter = 1; counter <= 4; counter++) {
-          Send7SEG(counter,0x40); // print ----
-        }
-        Serial.print("--- standby ---");
-      }
-      if (incomingByte == 9992) { // TODO : resume from standby
-        standby = false;
-        writing_word = false;
-        celsius = true;
-        blinking = false;
-        showing_trend = false;
-        Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, 1);
-        SerialMonitorPrint (Temperature_H, Decimal, IsPositive, true);
-      }
-      /*Feature #1: Display(on the pebble) whether the temperature has been rising or falling over the past hour.
-      This information will be sent from the arduino, and will be based on a linear regression on the temperature data from the past hour. */
-      if (incomingByte == 50) {// TODO feature 1
-        writing_word = false;
-        blinking = false;
-        showing_trend = true;
-        long xsum;
-        long ysum;
-        long xxsum;
-        long xysum;
-        double mNumerator;
-        double mDenominator;
-        double m;
-
-        //calculate summations over the past hour
-        for (int i = 0; i < j; i++){
-          ysum = ysum + temperature_stats[i][0];
-          xsum = xsum + i;
-          xysum = xysum + (i * temperature_stats[index][0]);
-          xxsum = xxsum + (i*i);
-        }
-
-        //calculate slope of regression
-        mNumerator = (j * xysum) - (xsum * ysum);
-        mDenominator = (j * xxsum) - (xsum * xsum);
-        m = mNumerator / mDenominator;
-
-        //send trend to computer
-        if (m > 0){
-          Serial.println("The temperature trend is increasing.");
-        } else if (m < 0){
-          Serial.println("The temperature trend is decreasing.");
-        } else {
-          Serial.println("The temperature has remained constant.");
-        }
-      } // end of feature 1
-
-      if (incomingByte == 52) {  // TODO feature 2 word 1
-        writing_word = true;
-        showing_trend = false;
-        Word_7SEG(0, 1, 2, 3); //  Oops
-      }
-      if (incomingByte == 9993) { // TODO feature 2 word 2
-        writing_word = true;
-        showing_trend = false;
-        Word_7SEG(4, 5, 6, 0); //  Hello
-      }
-      if (incomingByte == 9993) { // TODO feature 2 word 3
-        writing_word = true;
-        blinking = false;
-        showing_trend = false;
-        Word_7SEG(4, 0, 11, 10); //  HOLA
-      } // end of feature 2
-      if (incomingByte == 65) { // TODO feature 3 too cold
-        writing_word = false;
-        blinking = true;
-        showing_trend = false;
-        // blink blue
-        digitalWrite(BLUE, HIGH);
-        delay(1000);                       // wait for a second
-        digitalWrite(BLUE, LOW);
-        delay(1000);                       // wait for a second
-      }
-      if (incomingByte == 9993) { // TODO feature 3 temp is just right
-        writing_word = false;
-        blinking = true;
-        showing_trend = false;
-        // blink green
-        digitalWrite(GREEN, HIGH);
-        delay(1000);                       // wait for a second
-        digitalWrite(GREEN, LOW);
-        delay(1000);                       // wait for a second
-      }
-      else if (incomingByte == 9993) { // TODO feature 3 too hot
-        writing_word = false;
-        blinking = true;
-        showing_trend = false;
-        // blink red
-        digitalWrite(RED, HIGH);
-        delay(1000);                       // wait for a second
-        digitalWrite(RED, LOW);
-        delay(1000);                       // wait for a second
-      } // end of feature 3
-
-      byte tooHot = -1;
-      byte tooCold = -1;
-
-      if (incomingByte == 9999){ // TODO : feature 4 35 degrees too hot
-        showing_trend = false;
-        writing_word = false;
-        blinking = false;
-        tooHot = 35;
-      }
-
-      if (incomingByte == 9999){ // TODO: feature 4 30 degrees too hot
-        showing_trend = false;
-        writing_word = false;
-        blinking = false;
-        tooHot = 30;
-      }
-
-      if (incomingByte == 9999){ // TODO: feature 4 25 degrees too hot
-        writing_word = false;
-        showing_trend = false;
-        blinking = false;
-        tooHot = 25;
-
-      }
-
-      if (incomingByte == 9999) { // TODO: feature 4 0 is too cold
-        showing_trend = false;
-        blinking = false;
-        writing_word = false;
-        tooCold = 0;
-      }
-
-      if (incomingByte == 9999 ){ // TODO: feature 4 5 degrees is too cold
-        showing_trend = false;
-        writing_word = false;
-        blinking = false;
-        tooCold = 5;
-      }
-
-      if (incomingByte == 9999){ // TODO : feature 4 10 degrees is too cold
-        showing_trend = false;
-        writing_word = false;
-        blinking = false;
-        tooCold = 10;
-      }
-
-      if (incomingByte == 9990){ // TODO : feature 4 asking for a drink 9990 is "thirsty" option
-      showing_trend = false;
-      writing_word = false;
-      blinking = false;
-      if (tooHot == -1 || tooCold == -1) {
-        // error handling
-        Serial.print("No preference found.");
-      }
-      if (Temperature_H >= tooHot){
-        Serial.print("Have an iced tea!");
-      } else if (Temperature_H <= tooCold){
-        Serial.print("Have a hot cocoa!");
-      } else {
-        Serial.print("Have a glass of water!");
       }
     }
-    //    }
 
     /*Feature #4: From the pebble, the user will be able to select a temperature above which,
     it gets too hot for them, and another temperature below which it gets too cold for them.
